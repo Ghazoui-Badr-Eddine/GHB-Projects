0a651744b72a3915aedf6e4ccabb3df47f44bc03
Badr_E_GH <58095725+Ghazoui-Badr-Eddine@users.noreply.github.com>
2022-12-28 17:05:29 +0100

    Arduino Sketch File

---
+++ Security_System.ino
@@ -0,0 +1,32 @@
+#define SENSOR 3   // pin 2 for  sensor
+#define BUZZER 8   // pin 8 for buzzer to do start
+#define BUTTON 12  // pin 12 for button to stop the buzzer
+
+void setup() {  
+  Serial.begin(9600);
+  pinMode(SENSOR, INPUT);        //define SENSOR input pin
+  pinMode(BUZZER, OUTPUT);       //define BUZZER output pin
+  pinMode(BUTTON, INPUT_PULLUP); //define BUTTON input_pullup pin
+}
+
+void loop() {
+      Serial.print("hello");
+
+  int detected = digitalRead(SENSOR); // read Laser sensor
+  if( detected == HIGH)
+  {
+    Serial.print("intrusion");
+    digitalWrite(BUZZER,LOW);         // set the buzzer OFF
+    Serial.println("laser");
+  }else{
+        
+
+    digitalWrite(BUZZER,HIGH);        // set the buzzer ON
+    Serial.println("Intruder detected!");
+    if(!digitalRead(BUTTON)){
+      digitalWrite(BUZZER,LOW);       // set the buzzer OFF
+      Serial.println("laser");
+      }
+  }
+  delay(200);
+}
